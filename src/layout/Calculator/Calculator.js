0f10bfeca750b954fd979aab9971ff1d2a78cd86
mhizsean <adeyiesther01@gmail.com>
2020-01-30 18:36:38 +0100

    adjustes some lines

--- src/layout/Calculator/Calculator.js
+++ src/layout/Calculator/Calculator.js
@@ -18,11 +18,12 @@ class Calculator extends React.Component{
 
         if ( pressedButton === 'C' ) 
         return this.clear();
-        else if (( pressedButton >= '0' && pressedButton <= '9') || pressedButton === '.') 
-        equation += pressedButton;
+        else if (
+            ( pressedButton >= '0' && pressedButton <= '9') || pressedButton === '.') equation += pressedButton;
         else if (['+', '-', '*', '/', '%'].indexOf(pressedButton) !== -1) equation += ' ' + pressedButton + ' ';
         else if (pressedButton === '=') {
             try {
+                // eslint-disable-next-line no-eval
                 const evalResult = eval(equation);
                 const result = Number.isInteger(evalResult) ? evalResult : evalResult.toFixed(2);
                 this.setState({result});
